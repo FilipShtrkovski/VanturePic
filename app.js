c1c21febd3b4d52b634f0454485fa3372db04ac1
Filip <filip.strkovski13@gmail.com>
2025-01-22 16:48:37 +0100

    connnected to mongodb Atlas database

--- app.js
+++ app.js
@@ -9,20 +9,26 @@ const path = require('path')
 const ejsMeta = require('ejs-mate')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash')
 const LocalStrategy = require('passport-local')
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
+
 
 const usersRouts = require('./routes/users')
 const postRouts = require('./routes/posts')
 const commentRouts = require('./routes/comments')
 
-
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpressError')
 const User = require('./models/user')
+const { func } = require('joi')
 
-mongoose.connect('mongodb://127.0.0.1:27017/vanturePic')
+const dbUrl ='mongodb://127.0.0.1:27017/vanturePic'
+// process.env.DB_URL 
+mongoose.connect(dbUrl)
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -30,13 +36,27 @@ db.once("open", () => {
     console.log("Database connected")
 });
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret:'Thisisasecret',
+    touchAfter: 24 * 3600
+})
+
+store.on('error', function(e){
+    console.log('SESSION STORE ERROR',e)
+})
+
 const configSession = {
-    secret:'Thisisnotagoodsecret',
+    store,
+    secret:'Thisisasecret',
+    name: 'session',
     resave: false,
     saveUninitialized: true,
     cookie:{
-       expires: Date.now() + 1000 * 60 * 60 * 24 * 7, 
-       maxAge: + 1000 * 60 * 60 * 24 * 7
+        httpOnly: true,
+        secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, 
+        maxAge: + 1000 * 60 * 60 * 24 * 7
     } 
 }
 
@@ -51,6 +71,27 @@ app.use(express.static( path.join(__dirname, 'public')))
 
 app.use(session(configSession))
 app.use(flash())
+app.use(mongoSanitize());
+app.use(helmet());
+
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/daiuoiqvv/",
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -58,6 +99,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next)=>{
     res.locals.currentUser = req.user
